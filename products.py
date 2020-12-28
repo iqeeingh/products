65a214f012a05e649d1e4463fe8b72fe069d590e
Wayne <iqeeingh@gmail.com>
2020-12-28 22:08:08 +0800

    add file

---
+++ products.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 20 20:56:58 2020
+
+@author: wayne
+"""
+
+products = []
+
+with open ('product.csv', 'r') as f:
+    for line in f:
+        if '商品,價格' in line :
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+
+while True:
+    name = input('請輸入商品名稱：')
+    if name == 'q':
+        break
+    price = int(input('請輸入商品價格：'))
+    
+    products.append([name , price])
+print (products)
+
+for p in products:
+    print(p[0], '的價格是' ,p[1])
+    
+with open ('product.csv' , 'w' ) as f :
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
+    
