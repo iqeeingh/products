c48ad968711faa54768d0a4f67bc3d9d807e2916
Wayne <iqeeingh@gmail.com>
2021-01-05 23:07:45 +0800

    add import

--- products.py
+++ products.py
@@ -4,17 +4,22 @@ Created on Sun Dec 20 20:56:58 2020
 
 @author: wayne
 """
-
+import os
 products = []
+if os.path.isfile('product.csv'):
+    print('找到檔案')
+    with open ('product.csv', 'r') as f:
+        for line in f:
+            if '商品,價格' in line :
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
 
-with open ('product.csv', 'r') as f:
-    for line in f:
-        if '商品,價格' in line :
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
-
+else:
+    print('檔案遺失')
+        
+#使用者輸入
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
